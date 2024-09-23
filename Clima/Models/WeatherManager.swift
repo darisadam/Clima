ab34c1cc8604a8e04c8bbb6ad2bd510f3e834e88
damdaris <darisadam@upi.edu>
2024-09-23 12:03:15 +0700

    feat: add networking with API

---
+++ Clima/Models/WeatherManager.swift
@@ -0,0 +1,45 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Adam Daris Ryadhi on 23/09/24.
+//
+
+import Foundation
+
+struct WeatherManager {
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid={API KEY}&units=metric"
+    
+    func fetchWeather(cityName: String) {
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        
+        //1. Create a URL
+        if let url = URL(string: urlString) {
+
+            //2. Create a URLSesion
+            let session = URLSession(configuration: .default)
+            
+            //3. Give the session a task
+            let task = session.dataTask(with: url, completionHandler: handle(data: response: error: ))
+            
+            //4. Start the task
+            task.resume()
+        }
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?) {
+        if error != nil {
+            print(error!)
+            return
+        }
+        
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString)
+        }
+    }
+}
